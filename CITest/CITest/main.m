c53ee3f263babcf19c09fd1b5ef627188da372be
yangfeiyu <yangfeiyu@meituan.com>
2017-09-11 10:27:43 +0800

    init

---
+++ CITest/CITest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CITest
+//
+//  Created by yangfeiyu on 2017/9/11.
+//  Copyright © 2017年 yangfeiyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
